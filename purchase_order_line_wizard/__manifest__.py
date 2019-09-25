b6821b27f082881a5602a8e15a8ce682799b6412
dgm204 <diegogilmanez@gmail.com>
2019-09-25 16:54:54 +0200

    Añadido dia 8

--- purchase_order_line_wizard/__manifest__.py
+++ purchase_order_line_wizard/__manifest__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Library Management",
+    'name': "Purchase Order Line Wizard",
 
     'summary': """
-        Library Management
+        ---
         """,
 
     'description': """
@@ -16,19 +16,15 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Tools',
+    'category': 'Product',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['purchase'],
 
     # always loaded
     'data': [
-        'data/data.xml',
-        'security/groups.xml',
-        'security/ir.model.access.csv',
-        'views/library_view.xml',
-        'views/library_book_category_view.xml',
+        'wizard/purchase_order_line_wizard_view.xml',
     ],
     'installable': True,
     'application': True,
